cd86cad7a0bcb325220f075ffd6b7e6e3d1328bd
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2020-11-06 16:19:00 -0500

    safe math

--- Crowdsharing project.sol
+++ Crowdsharing project.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.25;
 
+import "./SafeMath.sol";
+
 /*
 
 ------------------------------------
@@ -413,41 +415,4 @@ contract TronFlowTRX {
     
      
     
-}
-
-
-library SafeMath {
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b);
-
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0);
-        uint256 c = a / b;
-
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-
-        return c;
-    }
-
 }
