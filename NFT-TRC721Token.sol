35909ca2ba7567f8e501ba7a06b1092a1c08e93d
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-09-02 12:51:26 -0500

    update

--- NFT-TRC721Token.sol
+++ NFT-TRC721Token.sol
@@ -1232,7 +1232,7 @@ contract TRC721Enumerable is Context, TRC165, TRC721, ITRC721Enumerable {
 }
 
 contract TRC721Token is TRC721, TRC721Enumerable, TRC721MetadataMintable {
-    constructor() public TRC721Metadata("Your Token Name", "YTN") {
+    constructor() public TRC721Metadata("Brutus Gallery", "BRGY") {
 
     }
 }
