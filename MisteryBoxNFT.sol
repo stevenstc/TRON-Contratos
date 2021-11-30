ebaa8eb7093ea5a265db71cf526f843cde5632b0
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-11-29 21:13:02 -0500

    Update MisteryBoxNFT.sol

--- MisteryBoxNFT.sol
+++ MisteryBoxNFT.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.8.0;
 
-// SPDX-License-Identifier: Apache 2.0 
+// SPDX-License-Identifier: Apache-2.0 
 
 interface ITRC721 {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -18,6 +18,64 @@ interface ITRC721 {
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) external;
+}
+
+contract dinamicArray{
+    
+    function actualizarArray(address[] memory oldArray)public pure returns ( address[] memory) {
+
+        //añade un espacio para un nuevo dato
+        address[] memory newArray =   new address[](oldArray.length+1);
+    
+        for(uint i = 0; i < oldArray.length; i++){
+            newArray[i] = oldArray[i];
+        }
+        
+        return newArray;
+    }
+
+    function borrarArray(address[] memory oldArray)public pure returns ( address[] memory) {
+
+        //borra los espacios que esten con address(0)
+        address[] memory newArray;
+        uint largo;
+
+        for(uint i = 0; i < oldArray.length; i++){
+            if(oldArray[i] != address(0)){
+                newArray = actualizarArray(newArray);
+                newArray[largo] = oldArray[i];
+                largo++;
+            }
+        }
+        
+        return newArray;
+    }
+    
+  
+  function example() public pure returns(address[] memory){
+      address[] memory network;
+      
+      network = actualizarArray(network);
+
+      network[0] = address(1);
+
+      network = actualizarArray(network);
+
+      network[1] = address(2);
+      
+      return network;
+  }
+
+  function example2() public pure returns(address[] memory){
+      address[] memory network = example();
+      
+      network = borrarArray(network);
+
+      network[0] = address(0);
+      
+      return network;
+  }
+  
 }
 
 contract RandomNumber{
